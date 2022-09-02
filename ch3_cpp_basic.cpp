c0137517f28377096be34f7936125605951b8645
Chartreuse Sylvex <a1085125@mail.nuk.edu.tw>
2022-09-03 00:21:34 +0800

    add ch3, temp afk

--- ch3_cpp_basic.cpp
+++ ch3_cpp_basic.cpp
@@ -12,10 +12,24 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < meow_str.size(); i++)
     {
-        meow_str[i] = meow_str[i] + 1;
+        meow_str[i] = meow_str[i] - 'a' + 'A';
     }
 
-    std::cout <<meow_str<< std::endl;
+    std::cout << meow_str << std::endl;
 
+    // section 3:
+
+    int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    
     return 0;
 }
